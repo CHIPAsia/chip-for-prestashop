2b1fec33a7bf6db2cf13a7c9aa7a3598db32c381
Wan Zulkarnain <wan@chip-in.asia>
2026-08-18 13:45:36 +0200

    Add Marketplace compliance items: .htaccess, readme_en.pdf, module_key
    Per PrestaShop Marketplace mandatory technical rules:
    - .htaccess root protection (Apache 2.4/2.2)
    - docs/readme_en.pdf English user guide
    - module_key placeholder in constructor
    - config.xml version synced to 1.0.1

--- chip/chip.php
+++ chip/chip.php
@@ -45,6 +45,9 @@ class Chip extends PaymentModule
     /** @var string */
     public $confirmUninstall;
 
+    /** @var string Module key provided by PrestaShop Addons (assigned at product registration) */
+    public $module_key = 'YOUR_MODULE_KEY_FROM_PRESTASHOP_ADDONS';
+
     /** @var string Module version used in the creator_agent header */
     const CREATOR_AGENT = 'PrestaShop: 1.0.1';
 
